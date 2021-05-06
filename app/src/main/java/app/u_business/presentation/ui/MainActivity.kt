b31baa25f7a80073638033e8089eb5b20d87061d
georgiy.ter-grigoryants <123456>
2021-05-06 16:07:20 +0300

    start

---
+++ app/src/main/java/app/u_business/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.u_business.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.u_business.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
