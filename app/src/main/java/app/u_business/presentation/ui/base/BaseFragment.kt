821f7da16933a613ecb3c7f1131e5a0451605b8a
avkgeo <avakya.georgiy00@gmail.com>
2021-05-09 15:24:39 +0300

    refactor

---
+++ app/src/main/java/app/u_business/presentation/ui/base/BaseFragment.kt
@@ -0,0 +1,38 @@
+package app.u_business.presentation.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<Binding: ViewDataBinding> : Fragment(){
+
+    val TAG = javaClass.simpleName
+
+    protected abstract val layoutId: Int
+
+    protected lateinit var binding: Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        initViewModel()
+    }
+
+    protected open fun initViewModel() {}
+
+    protected open fun initViews() {}
+
+}
