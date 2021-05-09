995582f6f2498fe24a9ee5bfa4663dda996b1816
katrinm02 <katrinm02gm@gmail.com>
2021-05-09 18:34:30 +0300

    add sign_in layouts

--- app/src/main/java/app/u_business/presentation/ui/MainActivity.kt
+++ app/src/main/java/app/u_business/presentation/ui/MainActivity.kt
@@ -7,6 +7,6 @@ import app.u_business.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_sign_in)
     }
 }
---
+++ app/src/main/java/app/u_business/presentation/ui/sign_in/SignIn1.kt
@@ -0,0 +1,38 @@
+package app.u_business.presentation.ui.sign_in
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import app.u_business.R
+import app.u_business.databinding.FragmentSignIn1Binding
+
+class SignIn1 : Fragment() {
+
+    private lateinit var binding: FragmentSignIn1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentSignIn1Binding.inflate(layoutInflater)
+
+        val navHostFragment =
+            requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.btnSignIn.setOnClickListener{
+            navController.navigate(SignIn1Directions.actionSignIn1ToSignIn2())
+        }
+
+        binding.btnSignUp.setOnClickListener {
+            navController.navigate(SignIn1Directions.actionSignIn1ToSignUp1())
+        }
+
+
+        return binding.root
+    }
+}
