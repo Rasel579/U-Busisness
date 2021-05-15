17264484e97975d8d193044223b0f44ce8992fc6
Daniil Dobrodeev <danil.dobrodeev@mail.ru>
2021-05-15 16:21:01 +0300

    add Gallery screen

--- app/src/main/java/app/u_business/presentation/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/app/u_business/presentation/ui/gallery/GalleryFragment.kt
@@ -1,4 +1,51 @@
 package app.u_business.presentation.ui.gallery
 
-class GalleryFragment {
+import app.u_business.R
+import app.u_business.databinding.FrGalleryBinding
+import app.u_business.presentation.ui.base.BaseFragment
+
+class GalleryFragment(override val layoutId: Int = R.layout.fr_gallery) :
+    BaseFragment<FrGalleryBinding>() {
+
+    private val picturesAdapter: PicturesAdapter by lazy { PicturesAdapter() }
+
+    override fun initViews() {
+        initRv()
+        mockAdapter()
+    }
+    private fun initRv() {
+        binding.galleryPicturesRv.adapter = picturesAdapter
+    }
+
+    private fun mockAdapter() {
+        picturesAdapter.data = listOf(
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        )
+    }
 }
