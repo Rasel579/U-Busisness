38652612172e7fd654710c63a74c30eb2655e5fa
Daniil Dobrodeev <danil.dobrodeev@mail.ru>
2021-05-25 09:27:39 +0300

    finish news screen

--- app/src/main/java/app/u_business/presentation/ui/news/NewsFragment.kt
+++ app/src/main/java/app/u_business/presentation/ui/news/NewsFragment.kt
@@ -1,3 +1,79 @@
 package app.u_business.presentation.ui.news
 
-class NewsFragment
+import app.u_business.R
+import app.u_business.databinding.FrNewsBinding
+import app.u_business.domain.model.News
+import app.u_business.presentation.ui.base.BaseFragment
+
+class NewsFragment(override val layoutId: Int = R.layout.fr_news) : BaseFragment<FrNewsBinding>() {
+    private val adapter: NewsAdapter by lazy { NewsAdapter() }
+
+    override fun initViews() {
+        initRv()
+        mockAdapter()
+    }
+
+    private fun initRv() {
+        binding.newsRv.adapter = adapter
+    }
+
+    private fun mockAdapter() {
+        adapter.data = listOf(
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+            News(
+                "",
+                "Мы принимаем участие в научном исследовании",
+                "18 февраля 19:00",
+                "Разнообразный и богатый опыт djsfksdkfldsflsdfdsfkdskfkdsk"
+            ),
+
+        )
+    }
+}
